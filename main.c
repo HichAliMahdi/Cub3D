b01259e68c5ea07e20fc250a425236a3cbbbeece
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-4.42heilbronn.de>
2025-05-12 16:37:30 +0200

    added simple ESC key to exit : next step starting the project

--- main.c
+++ main.c
@@ -6,13 +6,25 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 16:21:30 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/05/12 16:30:33 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:36:41 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(void)
+void	key_hook(mlx_key_data_t keydata, void* param)
+{
+	mlx_t*	mlx;
+
+	mlx = (mlx_t*)param;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+	{
+		printf("ESC pressed. Exiting.\n");
+		mlx_close_window(mlx);
+	}
+}
+
+int main(void)
 {
 	mlx_t*	mlx;
 
@@ -22,6 +34,7 @@ int	main(void)
 		fprintf(stderr, "Failed to initialize MLX\n");
 		return (EXIT_FAILURE);
 	}
+	mlx_key_hook(mlx, key_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
