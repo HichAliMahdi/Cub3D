8eac9ccdebab46a89447e24c830186df6b14beba
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-4.42heilbronn.de>
2025-05-13 18:05:06 +0200

    doing raycasting init

---
+++ raycasting/raycasting_init.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting_init.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 17:53:28 by hali-mah          #+#    #+#             */
+/*   Updated: 2025/05/13 18:04:45 by hali-mah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+/**
+ * Initialize a ray for the current screen column
+ */
+static void	set_ray_steps_x(t_game *game, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (game->player.pos_x - ray->map_x)
+			* ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - game->player.pos_x)
+			* ray->delta_dist_x;
+	}
+}
+
+static void	set_ray_steps_y(t_game *game, t_ray *ray)
+{
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (game->player.pos_y - ray->map_y)
+			* ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - game->player.pos_y)
+			* ray->delta_dist_y;
+	}
+}
+
+void	init_ray(t_game *game, t_ray *ray, int x)
+{
+	ray->camera_x = 2 * x / (double)SCREEN_WIDTH - 1;
+	ray->ray_dir_x = game->player.dir_x + game->player.plane_x * ray->camera_x;
+	ray->ray_dir_y = game->player.dir_y + game->player.plane_y * ray->camera_x;
+	ray->map_x = (int)game->player.pos_x;
+	ray->map_y = (int)game->player.pos_y;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+	set_ray_steps_x(game, ray);
+	set_ray_steps_y(game, ray);
+}
